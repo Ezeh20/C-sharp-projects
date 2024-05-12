707db8f6f03e225b9bdef6f4ee090ba793b98f35
Ezeh chijioke <chijioke@clivealliance.com>
2024-05-12 21:25:05 +0100

    prog: A console program that Writes the larger of two numbers

--- Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
+++ Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b46ac59cecfa2d7bf9e356170f800bb0911231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee719814eb13688ea309a5f020b58b973beced8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
