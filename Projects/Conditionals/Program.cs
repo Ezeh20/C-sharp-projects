d1a67ee161f481ba33bf937fe1744103ec30af17
Ezeh20 <ranter1570@gmail.com>
2024-05-13 23:27:33 +0100

    prog: A program that rolls a dice three times then displays a win or lose based on some conditions

--- Projects/Conditionals/Program.cs
+++ Projects/Conditionals/Program.cs
@@ -1,4 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int firstNumber;
+int secondNumber;
+int thirdNumber;
+int total;
 
+Random dice = new();
 
-Console.WriteLine("Hello, World!");
+firstNumber = dice.Next(1, 7);
+secondNumber = dice.Next(1, 7);
+thirdNumber = dice.Next(1, 7);
+
+total = firstNumber + secondNumber + thirdNumber;
+
+if ((firstNumber == secondNumber) || (firstNumber == thirdNumber) || (secondNumber == thirdNumber))
+{
+    total += 2;
+    Console.WriteLine(
+        $"You did a double {firstNumber} {secondNumber} {thirdNumber} total : {total}"
+    );
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("You lose");
+}
