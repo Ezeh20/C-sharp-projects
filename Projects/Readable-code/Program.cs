5c12f15926e98736d3801f57d1c68f78ca8eeaf8
Ezeh20 <ranter1570@gmail.com>
2024-05-22 02:10:50 +0100

    applied a block comment

--- Projects/Readable-code/Program.cs
+++ Projects/Readable-code/Program.cs
@@ -1,7 +1,9 @@
 ﻿string firstName = "Bob";
 int widgetsPurchased = 7;
 
-// Testing a change to the message.
-// int widgetsSold = 7;
-// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+/*
+Testing a change to the message.
+int widgetsSold = 7;
+Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+*/
 Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
