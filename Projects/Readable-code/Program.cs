fe94519c7cbde9719c4d8d8937d616332abe37c4
Ezeh20 <ranter1570@gmail.com>
2024-05-22 02:09:36 +0100

    learn: writing comments that is ignored by the compiler

--- Projects/Readable-code/Program.cs
+++ Projects/Readable-code/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string firstName = "Bob";
+int widgetsPurchased = 7;
+
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
