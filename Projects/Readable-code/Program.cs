2599c657e1b7f59c89060f90526198b0e99ecae7
Ezeh20 <ranter1570@gmail.com>
2024-05-22 02:15:00 +0100

    working with white spaces

--- Projects/Readable-code/Program.cs
+++ Projects/Readable-code/Program.cs
@@ -1,9 +1,15 @@
-﻿string firstName = "Bob";
-int widgetsPurchased = 7;
-
-/*
-Testing a change to the message.
-int widgetsSold = 7;
-Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
-*/
-Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+﻿Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } else {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
--- Projects/Readable-code/obj/Debug/net8.0/Readable-code.AssemblyInfo.cs
+++ Projects/Readable-code/obj/Debug/net8.0/Readable-code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readable-code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03edb20fd0e89af81c384748a5bc37f3809a1f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c12f15926e98736d3801f57d1c68f78ca8eeaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readable-code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readable-code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
