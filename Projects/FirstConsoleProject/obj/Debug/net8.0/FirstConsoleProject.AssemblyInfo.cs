292d51968b3140f52237b9597a83d737a18fd908
Ezeh20 <ranter1570@gmail.com>
2024-05-29 23:52:00 +0100

    prog: created a new console project for the array challenge

--- Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
+++ Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f909e8f5bef08a689360d81fbbe64570c0e6550c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739320a03c28da7435733a7f53ce7362fbb09f1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
