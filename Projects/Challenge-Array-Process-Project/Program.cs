88c301f29218321fb66f9fb4dd99226e21c10b6b
Ezeh20 <ranter1570@gmail.com>
2024-05-30 23:53:19 +0100

    prog: project setup for the grading improved grading system

--- Projects/Challenge-Array-Process-Project/Program.cs
+++ Projects/Challenge-Array-Process-Project/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+This console project automates a grading process
+The final results prints the student's name, score, grade and extra credit sore
+The programm should also show the effect the extra credit on the student's final score
+*/
+
+
+
+int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
--- Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
+++ Projects/FirstConsoleProject/obj/Debug/net8.0/FirstConsoleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739320a03c28da7435733a7f53ce7362fbb09f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292d51968b3140f52237b9597a83d737a18fd908")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstConsoleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
