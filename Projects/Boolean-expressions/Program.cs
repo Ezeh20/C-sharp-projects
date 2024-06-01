984ce85668c614f9f419d5f0b70afe2481871b1d
Ezeh20 <ranter1570@gmail.com>
2024-06-01 01:18:23 +0100

    exe: went through conditional operator, expression, statement, operators, logical nagation

--- Projects/Boolean-expressions/Program.cs
+++ Projects/Boolean-expressions/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string pangram = "The quick brown fox jumps over the lazy dog.";
+bool final = pangram.Contains(" Fox".Trim().ToLower());
+
+int saleAmount = 1000;
+int discount = saleAmount > 1000 ? 100 : 50;
+
+int toss = new Random().Next(0, 2);
+string result = toss == 0 ? "heads" : "tails";
+Console.WriteLine(result);
