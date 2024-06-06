d57369b4213be44b84407cd3e79f63cf868b83aa
Ezeh20 <ranter1570@gmail.com>
2024-06-06 01:11:19 +0100

    exe: do-while, while loop

---
+++ Projects/More-Loops-Do-while-while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Random number = new();
+int current = number.Next(1, 11);
+
+do
+{
+    current = number.Next(1, 11);
+    if (current >= 8)
+        continue;
+    Console.WriteLine(current);
+} while (current != 10);
+
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = number.Next(1, 11);
+}
+
+Console.WriteLine($"Last number : {current}");
