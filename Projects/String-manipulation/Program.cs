582c9ecae7aa2864529c8d666d62b791209a3cc1
Ezeh20 <ranter1570@gmail.com>
2024-07-03 00:19:59 +0100

    String manipulation

---
+++ Projects/String-manipulation/Program.cs
@@ -0,0 +1,20 @@
+﻿string message = "Find what is (inside the parentheses)";
+
+int opening = message.IndexOf("(");
+int closing = message.IndexOf(")");
+opening++;
+
+int length = closing - opening;
+
+// Console.Write(message.Substring(opening, length));
+
+string message2 = "What is the value <span>between the tags</span>?";
+
+const string opn = "<span>";
+const string cls = "</span>";
+
+int starting = message2.IndexOf(opn);
+int ending = message2.IndexOf(cls);
+
+starting += opn.Length;
+Console.WriteLine(message2.Substring(starting, (ending - starting)));
